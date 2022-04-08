3b0c0a536df8bcc9536277f73fc3a7ebffec29a1
Matias <micea@uc.cl>
2022-04-08 19:47:09 -0400

    feat: add map and tiles

---
+++ src/TileMap.js
@@ -0,0 +1,51 @@
+export default class TileMap {
+  constructor(tileSize){
+    this.tileSize = tileSize;
+    this.yellowDot = new Image();
+    this.yellowDot.src = "../img/yellowdot.png";
+
+    this.wall = new Image();
+    this.wall.src = "../img/wall.png";
+
+    this.pac0 = new Image();
+    this.pac0.src = "../img/pac0.png";
+  }
+
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  draw(ctx) {
+    for(let row = 0; row < this.map.length; row++) {
+      for(let col = 0; col < this.map[row].length; col++) {
+        if(this.map[row][col] === 1) {
+          this.#drawImage(this.wall, col * this.tileSize, row * this.tileSize, ctx);
+        } else if(this.map[row][col] === 0) {
+          this.#drawImage(this.yellowDot, col * this.tileSize, row * this.tileSize, ctx);
+        }
+        // else if(this.map[row][col] === 0) {
+        //   this.#drawImage(this.pac0, col * this.tileSize, row * this.tileSize, ctx);
+        // }
+      }
+    }
+  }
+
+  #drawImage(img, x, y, ctx) {
+    ctx.drawImage(img, x, y, this.tileSize, this.tileSize);
+  }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
+}
